1b77ac3016f9086baa5783f9f43d012fa770dc3d
anshoria <anshoriakbar11@gmail.com>
2021-06-19 05:23:43 +0800

    First Commit

---
+++ app/src/main/java/com/example/testbarang/MainActivity.java
@@ -0,0 +1,38 @@
+package com.example.testbarang;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import com.example.testbarang.R;
+
+public class MainActivity extends AppCompatActivity {
+    Button tambahbtn,lihatbtn;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        tambahbtn = findViewById(R.id.btnInsert);
+        lihatbtn = findViewById(R.id.btnSelect);
+
+        tambahbtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent =  new Intent(MainActivity.this,TambahTeman.class);
+                startActivity(intent);
+            }
+        });
+
+        lihatbtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+    }
+}
